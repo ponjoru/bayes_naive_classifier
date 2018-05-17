386c7324812855d918df2ab4dc95b496f274f6ec
Ekaterina <36381415+EkaterinaMikhailova@users.noreply.github.com>
2018-05-18 01:22:21 +0300

    Added test for data processing

--- test/main.cpp
+++ test/main.cpp
@@ -7,6 +7,7 @@
 #include"stopwords.cpp"
 #include"DataProcess.h"
 #include"DataProcess.cpp"
+#define LANGUAGE "russian"
 
 using namespace std;
 Q_DECLARE_METATYPE(wstring)
@@ -33,6 +34,17 @@ vector<wstring> stringToVec(wstring wstr)
 	stringToV.push_back(tmp);
 	return stringToV;
 }
+
+vector<wstring> loadData(string filename)
+{
+	wifstream stream(filename);
+	stream.imbue(locale(LANGUAGE));
+	wstring line;
+	vector<wstring> res;
+	getline(stream, line);
+	res = stringToVec(line);
+	return res;
+}
 class TestModules : public QObject
 {
 	Q_OBJECT
@@ -42,6 +54,9 @@ class TestModules : public QObject
 
 	void test_deleteStopWords_data();
 	void test_deleteStopWords();
+
+	void test_processingWords_data();
+	void test_processingWords();
 
 };
 
@@ -153,6 +168,11 @@ void TestModules::test_stemmer_data()
 	wstring word23(L"пароход");
 	wstring stem23(L"пароход");
 	QTest::newRow("word_23_nothing is deleted") << word23 << stem23;
+
+	wstring word24(L"сумел");
+	wstring stem24(L"сумел");
+	QTest::newRow("word_23_nothing is deleted") << word24 << stem24;
+
 }
 void TestModules::test_stemmer()
 {
@@ -166,8 +186,8 @@ void TestModules::test_deleteStopWords_data()
 {
 	QTest::addColumn<vector<wstring>>("String_input");
 	QTest::addColumn<vector<wstring>>("String_without_stop_words");
-	vector<wstring> inputStr1, inputStr2, inputStr3, inputStr4, inputStr5, inputStr6, inputStr7;
-	vector<wstring> withoutStop1, withoutStop2, withoutStop3, withoutStop4, withoutStop5, withoutStop6, withoutStop7;
+	vector<wstring> inputStr1, inputStr2, inputStr3, inputStr4, inputStr5, inputStr6, inputStr7, inputStr8;
+	vector<wstring> withoutStop1, withoutStop2, withoutStop3, withoutStop4, withoutStop5, withoutStop6, withoutStop7, withoutStop8;
 
 	// стоп слова в начале строки
 	inputStr6 = stringToVec(L"в петербурге открылась выставка");
@@ -195,8 +215,8 @@ void TestModules::test_deleteStopWords_data()
 	// стоп слова в середине строки
 	inputStr5 = stringToVec(L"связаться с редакцией или сообщить");
 	withoutStop5 = stringToVec(L"связаться редакцией сообщить");
-
-
+	inputStr8 = stringToVec(L"сдерживать рост цен пришлось резервному банку который сумел");
+	withoutStop8 = stringToVec(L"сдерживать рост цен пришлось резервному банку сумел");
 	QTest::newRow("string_1_at_the_beginning_middle") << inputStr1 << withoutStop1;
 	QTest::newRow("string_2_at_four_in_a_row") << inputStr2 << withoutStop2;
 	QTest::newRow("string_3_at_the_end") << inputStr3 << withoutStop3;
@@ -204,6 +224,8 @@ void TestModules::test_deleteStopWords_data()
 	QTest::newRow("string_5_at_the_middle") << inputStr5 << withoutStop5;
 	QTest::newRow("string_6_at_the_beginning") << inputStr6 << withoutStop6;
 	QTest::newRow("string_7_all") << inputStr7 << withoutStop7;
+	QTest::newRow("string_8") << inputStr8 << withoutStop8;
+
 
 }
 void TestModules::test_deleteStopWords()
@@ -214,6 +236,46 @@ void TestModules::test_deleteStopWords()
 	stopWord.removeWord(&String_input);
 	QCOMPARE(String_input, String_without_stop_words);
 }
+void TestModules::test_processingWords_data() 
+{
+	QTest::addColumn<vector<wstring>>("Words_input");
+	QTest::addColumn<vector<wstring>>("Words_processed");
+
+	vector<wstring> inputStr1, inputStr2, inputStr3, inputStr4, inputStr5, inputStr6, inputStr7;
+	vector<wstring> processedStr1, processedStr2, processedStr3, processedStr4, processedStr5, processedStr6, processedStr7;
+
+	inputStr1 = stringToVec(L"он однажды говорил шесть раз подряд по многочисленным");
+	QTest::newRow("string_1_all_stop_words") << inputStr1 << processedStr1;
+
+	inputStr2 = stringToVec(L"енот ест пароход");
+	processedStr2 = stringToVec(L"енот ест пароход");
+
+	QTest::newRow("string_2_without_changes") << inputStr2 << processedStr2;
+
+	inputStr3 = loadData("test_processed1.txt");
+	processedStr3 = loadData("test_processed_res1.txt");
+	QTest::newRow("string_3") << inputStr3 << processedStr3;
+
+	inputStr4 = loadData("test_processed3.txt");
+	processedStr4 = loadData("test_processed_res3.txt");
+	QTest::newRow("string_4") << inputStr4 << processedStr4;
+
+	inputStr5 = loadData("test_processed4.txt");
+	processedStr5 = loadData("test_processed_res4.txt");
+	QTest::newRow("string_5") << inputStr5 << processedStr5;
+
+	inputStr6 = loadData("test_processed5.txt");
+	processedStr6 = loadData("test_processed_res5.txt");
+	QTest::newRow("string_6") << inputStr6 << processedStr6;
+}
+void TestModules::test_processingWords()
+{
+	QFETCH(vector<wstring>, Words_input);
+	QFETCH(vector<wstring>, Words_processed);
+	DataProcess process;
+	process.processingWords(&Words_input);
+
+	QCOMPARE(Words_input, Words_processed);
+}
 QTEST_MAIN(TestModules)
 #include "main.moc"
-
