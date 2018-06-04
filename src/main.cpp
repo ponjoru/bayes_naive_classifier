f470b2d3875cd6b9394187c530608423985500e7
ponjoru <36381961+ponjoru@users.noreply.github.com>
2018-06-05 02:23:43 +0300

    console cp1251 usage added

--- src/main.cpp
+++ src/main.cpp
@@ -1,11 +1,15 @@
 ﻿#include "BayesClassifier.h"
-
+#include <iostream>
+#include <locale>
+#include <Windows.h>
 /**
 Точка входа программы. Инициализация класса BayesClassifier. Вычисление результатов. Печать результатов.
 */
 int main(void)
 {
-	//std::locale::global(std::locale(""));
+	setlocale(LC_ALL, "Russian");
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
 	std::ios::sync_with_stdio(false);
 	
 	BayesClassifier b_cl;
